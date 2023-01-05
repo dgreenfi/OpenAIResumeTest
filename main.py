4a370647c6e8ecef9fac814b45571ec870bb3ffb
davidgreenfield <dgreenfi@gmail.com>
2023-01-05 10:31:57 -0500

    Updates to fix examples and add a few comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+#important packages to load key and call API
 import os
 import openai
 from dotenv import load_dotenv
@@ -20,6 +21,7 @@ def auth_openai():
     print("Do Later")
 
 def example_query():
+    # where my key is stored
     dotenv_path = Path('OAI.env')
     load_dotenv(dotenv_path=dotenv_path)
     openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -29,8 +31,7 @@ def example_query():
         max_tokens=128,
         temperature=0.5)
     print(resp)
-    completion = openai.Completion.create(engine="ada", prompt="Hello world")
-    print(completion)
+
 
 def basequery():
     dotenv_path = Path('OAI.env')
@@ -38,12 +39,10 @@ def basequery():
     openai.api_key = os.getenv('OPENAI_API_KEY')
     resp = openai.Completion.create(
         model="davinci:ft-personal-2022-12-22-19-32-33",
-        prompt="write a cover lever for David Greenfield's highlighting his work at Veeva and Komodo",
+        prompt="write a cover lever for David Greenfield highlighting his work at Veeva and Komodo",
         max_tokens=128,
-        temperature=0.5)
+        temperature=0.9)
     print(resp)
-    completion = openai.Completion.create(engine="ada", prompt="Hello world")
-    print(completion)
 
 def train_model(basemodel,trainset,num_epochs):
     dotenv_path = Path('OAI.env')
@@ -67,6 +66,6 @@ def train_model(basemodel,trainset,num_epochs):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     #train_model('text-davinci-003', 'resume_train.JSONL',3)
-    example_query()
-    #basequery()
+    #example_query()
+    basequery()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
